e9b15d17f9e0b62d6468929f441d71fe0019a018
TianyouZhang721 <zhangty_0721@163.com>
2020-05-31 16:44:47 +0800

    歌词

--- src/pages/Play/Play.js
+++ src/pages/Play/Play.js
@@ -8,6 +8,10 @@ class Play extends Component {
             picUrl: "", // 圆圈的图片
             url: "", // 歌曲地址
             flag: true, // 控制按钮的显示与隐藏
+            songName: "",
+            singerName: "",
+            timeArr: [],
+            lyricArr: []
         }
         this.getDetail = this.getDetail.bind(this)
         this.getUrl = this.getUrl.bind(this)
@@ -34,8 +38,20 @@ class Play extends Component {
         ]).then(
             this.$http.spread((res1, res2, res3) => {
                 console.log(res1)
-                console.log(res2)
+                console.log(res2.data.lrc.lyric)
                 console.log(res3)
+                var lyric = res2.data.lrc.lyric// 歌词字符串
+                var arr = lyric.split(/\n/)
+                console.log(arr)
+                // 分出两个数组，一个存时间，一个存歌词
+                let timeArr = arr.map(item => {
+                    return item.slice(1, 10)
+                })
+                let lyricArr = arr.map(item => {
+                    return item.slice(11)
+                })
+                console.log(timeArr)
+                console.log(lyricArr)
                 this.setState({
                     playStyle: {
                         background: `url(${res1.data.songs[0].al.picUrl}) no-repeat center center`,
@@ -43,7 +59,11 @@ class Play extends Component {
                         filter: "blur(10px)"
                     },
                     picUrl: res1.data.songs[0].al.picUrl,
-                    url: res3.data.data[0].url
+                    url: res3.data.data[0].url,
+                    songName: res1.data.songs[0].al.name,
+                    singerName: res1.data.songs[0].ar[0].name,
+                    timeArr,
+                    lyricArr
                 })
             })
         )
@@ -64,7 +84,7 @@ class Play extends Component {
         }
     }
     render() {
-        let { playStyle, picUrl, url, flag } = this.state
+        let { playStyle, picUrl, url, flag, songName, singerName, lyricArr } = this.state
         return (
             <div className="play-box">
                 <div className="bg-box"  style={ playStyle }></div>
@@ -79,6 +99,20 @@ class Play extends Component {
                         flag && <i className="iconfont icon-bofang" onClick={this.play.bind(this)}></i>
                     } */}
                     <i className={flag ? "iconfont icon-bofang" : "iconfont icon-bofang opa"} onClick={this.play.bind(this)}></i>
+                </div>
+                <div className="lyric-box">
+                    <p>{songName} - 
+                        <span>{singerName}</span>
+                    </p>
+                    <div className="box">
+                        <ul>
+                            {
+                                lyricArr.map((item, index) => {
+                                    return <li key={index}>{item}</li>
+                                })
+                            }
+                        </ul>
+                    </div>
                 </div>
                 <audio src={url} ref={(audio) => {
                     this.audio = audio
